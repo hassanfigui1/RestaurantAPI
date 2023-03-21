b2632473cb09502e7f79232b2c46f8063a6d6d0e
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-03-21 19:39:31 +0000

    working on displaying menus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddSectionComponent } from './add-section/add-section.component';
+import { SectionComponent } from './section/section.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'add-menu', component:AddSectionComponent},
+  {path:'menu-items', component:SectionComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
