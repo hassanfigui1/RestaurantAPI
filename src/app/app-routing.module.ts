630e71a20fa196b4fc9185cb58ecfc7025759498
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-03-25 18:43:14 +0000

    adding activateRoute for modifying menu item

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddSectionComponent } from './add-section/add-section.component';
 import { SectionComponent } from './section/section.component';
+import { UpdateMenuComponent } from './update-menu/update-menu.component';
 
 const routes: Routes = [
   {path:'add-menu', component:AddSectionComponent},
-  {path:'menu-items', component:SectionComponent}
-
+  {path:'menu-items', component:SectionComponent},
+  {path:'update-menu/:id', component:UpdateMenuComponent},
+ 
 ];
 
 @NgModule({
--- src/app/update-menu/update-menu.component.ts
+++ src/app/update-menu/update-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-update-menu',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-menu.component.css']
 })
 export class UpdateMenuComponent {
+
+  menu_items : any;
+  id: any;
+
+  constructor(private act: ActivatedRoute){}
+
+  updateMenuItem(){}
+
+  ngOnInit():void{
+    this.id = this.act.snapshot.paramMap.get('id');
+  
+  }
+  
 
 }
